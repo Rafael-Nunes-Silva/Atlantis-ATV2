e8b126c6135a6d93233e2dab60be5a302568442a
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-03-08 13:10:15 -0300

    Listagem do titular de um dependente

--- src/ts/processos/tipoListagemClientes.ts
+++ src/ts/processos/tipoListagemClientes.ts
@@ -1,6 +1,7 @@
 import Processo from "../abstracoes/processo";
 import MenuTipoListagemClientes from "../menus/menuTipoListagemClientes";
 import ListagemDependentes from "./listagemDependentes";
+import ListagemTitularDependente from "./listagemTitularDependente";
 import ListagemTitulares from "./listagemTitulares";
 
 export default class TipoListagemClientes extends Processo {
@@ -19,6 +20,10 @@ export default class TipoListagemClientes extends Processo {
                 break;
             case 2:
                 this.processo = new ListagemDependentes();
+                this.processo.processar();
+                break;
+            case 3:
+                this.processo = new ListagemTitularDependente();
                 this.processo.processar();
                 break;
             default:
