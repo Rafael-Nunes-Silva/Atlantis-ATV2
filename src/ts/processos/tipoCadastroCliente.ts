ab54104679528892e5b8d2be515b4bebafcd2e17
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-03-08 13:13:18 -0300

    Cadastro de dependente

--- src/ts/processos/tipoCadastroCliente.ts
+++ src/ts/processos/tipoCadastroCliente.ts
@@ -1,5 +1,6 @@
 import Processo from "../abstracoes/processo";
 import MenuTipoCadastroCliente from "../menus/menuTipoCadastroCliente";
+import CadastroClienteDependente from "./cadastroClienteDependente";
 import CadastroClienteTitular from "./cadastroClienteTitular";
 
 export default class TipoCadastroCliente extends Processo {
@@ -9,15 +10,19 @@ export default class TipoCadastroCliente extends Processo {
     }
     processar(): void {
         this.menu.mostrar()
-        this.opcao = this.entrada.receberNumero('Qual opção desejada?')
+        this.opcao = this.entrada.receberNumero("Qual opção desejada?")
         
         switch (this.opcao) {
             case 1:
                 this.processo = new CadastroClienteTitular()
                 this.processo.processar()
                 break
+            case 2:
+                this.processo = new CadastroClienteDependente()
+                this.processo.processar()
+                break
             default:
-                console.log('Opção não entendida :(')
+                console.log("Opção não entendida :(")
         }
     }
 }
